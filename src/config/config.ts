bc6667b64e1f62d753cbb8f1699e282bac3b18c5
prasish <prasishshrestha0099@gmail.com>
2023-04-26 10:51:42 +0545

    fix some url

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const username = process.env.username||"";
 const password = process.env.password||"";
 
-const MONGO_URL = `mongodb+srv://Prasish_shrestha:nayathimi0099@nodeexpressprojects.yrfczb4.mongodb.net/Book?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL || `mongodb+srv://${username}:${password}@nodeexpressprojects.yrfczb4.mongodb.net/Book?retryWrites=true&w=majority`;;
 
 const PORT = process.env.PORT || 3000;
 
